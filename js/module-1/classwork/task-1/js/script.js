e645724f141448f78e27c834cef53a62b535f71d
Kondrat1991 <kondrat19911708@gmail.com>
2018-04-11 11:16:20 +0300

    js 1 homework

---
+++ js/module-1/classwork/task-1/js/script.js
@@ -0,0 +1,137 @@
+/*
+var x =5;
+console.log('var x', x);
+console.log(x);
+let y = 10;
+console.log(y);
+const z = 50;
+console.log(z);
+*/
+
+// var x =5;
+// x = 20;
+// console.log(x);
+
+// let b = 10;
+// let a = 'text';
+// let z = true;
+// let f = false;
+
+
+// let a = {
+//     name:'Eminem',
+//     age: 12,
+//     isRaper: false,
+//
+//
+// };
+//
+// console.log(a);
+//
+// let jigul = {
+//     wheels:3,
+//     color: 'red',
+//     isCar:false,
+//     condition: false,
+//
+// };
+//
+// console.log(jigul);
+//
+//
+// let user  = {
+//
+//     name:'Ivan',
+//     age: 5,
+//     gender: 'male',
+//     mail: 'abc@mail',
+//     isLogin: false,
+//
+//     private: {
+//         password:'zdtrh',
+//         creditCard:122414235,
+//     },
+//
+// };
+
+// console.log(user);
+//
+//
+//
+// let z = Symbol('a');
+// let b = Symbol('a');
+//
+// console.log(z === b);
+
+/*
+number
+string
+boolean
+null
+undefined
+object
+symbol
+*/
+
+
+
+// let b = undefined;
+// console.log(typeof b);
+
+// alert('hello world');
+// document
+// console
+
+// confirm('are you student?') = boolean;
+// prompt()= string;
+
+// let name  = prompt('Enter your name');
+// let  age = prompt('Enter your age');
+
+// Lorem ipsum dolor sit amet, consectetur
+// adipisicing elit. At, eaque eum ipsam laboriosam
+// molestiae nobis quisquam ullam veniam! Illo, temporibus.
+
+
+// let num = 3;
+// alert(num);
+
+
+/*let a = 10;
+let b = 2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);*/
+
+
+/*let a =17;
+let b =7;
+let d = 7;
+let c = (a-b);
+console.log(c);
+result = c + d;
+console.log(result);*/
+
+
+// let b ='Hello';
+//
+// console.log(b.endsWith('lo'));
+
+
+
+//
+// let b ='Lorem ipsum dolor sit amet, consectetur\n' +
+//     '    adipisicing elit. At, eaque eum ipsam laboriosam\n' +
+//     '    molestiae nobis quisquam ullam veniam! Illo, temporibus.';
+//
+// // console.log(b.startsWith('lo'));
+// console.log(b.includes('rem'));
+// console.log(b.indexOf('r'));
+//
+// document.write(b.repeat('5'));
+// console.log(b);
+
+
+
