c954e18dbd4d1d9ca1350ffc05dbf2fce45a8b3c
Kondrat1991 <kondrat19911708@gmail.com>
2018-04-11 11:28:11 +0300

    uncomment homework

---
+++ js/module-1/classwork/task-2/script.js
@@ -0,0 +1,209 @@
+/*let question2 = prompt('What is your name?');
+console.log(question2);
+alert('Ваше имя '+question2+'!');
+console.log(typeof question2);
+
+let number = 0;
+number= prompt('Введіть число ', ' ');
+let square = number*number;
+alert ('Квадрат числа = ' + square);
+
+let o = undefined;
+console.log(typeof o);*/
+
+
+
+
+// task 8;
+/*let hour = 3600;
+console.log(hour);
+let day = hour*24;
+console.log(day);
+let month = hour*741;
+console.log(month);*/
+
+// task9
+/*let hour = '12';
+let minute = '14';
+let second = '45';
+
+console.log(`${hour}:${minute}:${second}`);*/
+
+// task 10;
+/*let number = 2;
+let square = number*number;
+console.log(square);
+alert(square);*/
+
+
+// task 11
+
+// var num = 47;
+// num = num + 7;
+// num = num - 18;
+// num = num * 10;
+// num = num / 15;
+// alert (num);
+
+
+// task 12
+/*var num = 47;
+num +=  7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert (num);*/
+
+// task 13
+
+/*var num = 10;
+num = num++;
+num = num++;
+num = num--;
+alert (num);*/
+
+
+
+
+/*let a = 'Hello World';
+alert(a);*/
+
+
+// task 14
+
+/*let str1 = 'Hello';
+let str2 = 'World';
+console.log(`${str1} ${str2}`);*/
+
+
+
+// task 15
+
+/*let name = prompt('Please enter your name :', '');
+let age = prompt('Please enter your age:', '');
+alert('Привіт я '+name+' мені '+age+'!');*/
+
+
+/*task 16*/
+
+/*let str = 'abcde';
+console.log(str[0]);
+console.log(str[2]);
+console.log(str[4]);*/
+
+// task 17
+
+/*let num = '12345';
+console.log((+num[0])+(+num[1])+(+num[2])+(+num[3])+(+num[4]));
+console.log(num[0]-num[1]-num[2]-num[3]-num[4]);
+console.log(num[0]*num[1]*num[2]*num[3]*num[4]);
+console.log(num[0]/num[1]/num[2]/num[3]/num[4]);*/
+
+
+// task 18
+
+/*let row = 'js';
+console.log(row.toUpperCase());*/
+
+// task 19
+
+/*let row = 'JS';
+console.log(row.toLowerCase());*/
+
+
+// task 20
+
+/*let row ='я вчу javascript!'
+console.log(row.length);*/
+
+// task 21
+/*let row ='я вчу javascript!'
+let word1=(row.substr(1,4))
+let word2=(row.substr(6,15))
+console.log(word1);
+console.log(word2);*/
+
+// task 22
+
+/*let row ='я вчу javascript!';
+console.log(row.indexOf('вчу'));*/
+
+// task 23
+
+/*let row ='я вчу javascript!';
+let splitWord = row.split(' ');
+console.log(splitWord);
+console.log(splitWord.join('+'));*/
+
+
+
+// task 24
+
+/*let row ='я вчу javascript!';
+let splitWord = row.split('');
+console.log(splitWord);*/
+
+// task 25
+
+/*let firstName = prompt('Введіть ім\'я');
+let lastName = prompt('Введіть прізвище?');
+let age = prompt('Введіть вік?');
+
+alert(`"Мене звати ${firstName} моє прізвище ${lastName} і мені стукноло ${age} рочків`);*/
+
+
+// task 26
+/*let phrase = 'Hello World';
+console.log(phrase[0]);
+console.log(phrase[4]);
+console.log(phrase[5]);*/
+
+
+// task 27
+
+/*let phrase1 = 'Hello World';
+let phrase2 = 'lol';
+let phrase3 = 'not that time';
+let space =' ';
+
+
+console.log(phrase1.concat(space, phrase2, space, phrase3));*/
+
+// task 28
+
+// let phrase = 'Hello world, welcome to the universe.';
+// console.log(phrase.endsWith('universe.'));
+// console.log(phrase.endsWith('universe'));
+//
+// // &task29
+// console.log(phrase.startsWith('universe.'));
+// console.log(phrase.startsWith('Hello'));
+
+
+// task 30
+
+
+/*let row ='я вчу javascript!';
+console.log(row.includes('javascript'));
+console.log(row.includes('CSS'));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
